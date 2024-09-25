ec86213b0e16064d16d6a42cf7f8eb19e14e7c72
yueht23 <yueht17@gmail.com>
2024-09-25 13:22:22 +0800

    feat:添加了reset_info_port,sync_info_python_to_info两个接口用于测试

--- surgery_brain/urls.py
+++ surgery_brain/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('first_schedule', views.get_first_schedule),
     path('sec_schedule', views.get_sec_schedule),
 
+    # 如下接口只是为了方便测试，实际使用时不需要
+    path('reset_info_port', views.reset_info_port),
+    path('sync_info_python_to_info', views.sync_info_python_to_info),
+    path('gantt_chart', views.gantt_chart),
 ]
--- surgery_brain/views.py
+++ surgery_brain/views.py
@@ -27,21 +27,63 @@ def get_sec_schedule(request):
     logger = Logger(__name__).get_logger()
     if request.method == 'POST':
         result = []
-        # query_condition = json.loads(request.body)
-        # case_date = query_condition['date']
-        #
-        # logger.info("sec schedule and  case_date: " + case_date)
-        # do_import_surgery(date=case_date)
-        #
-        # logger.info("init schedule object")
-        # my_schedule = schedule.Schedule(shedule_date=case_date)
-        #
-        # logger.info("pre sec schedule")
-        # my_schedule.pre_sec_schedule()
-        #
-        # logger.info("do sec schedule")
-        # result.append(my_schedule.do_sec_schedule())
-        #
-        # logger.info("sec schedule is done")
+        query_condition = json.loads(request.body)
+        case_date = query_condition['date']
+
+        logger.info("sec schedule and  case_date: " + case_date)
+
+        logger.info("init schedule object")
+        my_schedule = Schedule(schedule_date=case_date)
+
+        my_schedule.schedule_sec()
+        logger.info("sec schedule is done")
+        return HttpResponse(json.dumps(result))
+    return HttpResponse("请求错误")
+
+
+def reset_info_port(request):
+    logger = Logger(__name__).get_logger()
+    if request.method == 'POST':
+        result = []
+        query_condition = json.loads(request.body)
+        case_date = query_condition['date']
+
+        logger.info("reset info port and  case_date: " + case_date)
+
+        my_schedule = Schedule(schedule_date=case_date)
+        my_schedule.sio.reset_info_port()
+        logger.info("reset info port is done")
+        return HttpResponse(json.dumps(result))
+    return HttpResponse("请求错误")
+
+
+def sync_info_python_to_info(request):
+    logger = Logger(__name__).get_logger()
+    if request.method == 'POST':
+        result = []
+        query_condition = json.loads(request.body)
+        case_date = query_condition['date']
+
+        logger.info("sync info python to info and  case_date: " + case_date)
+
+        my_schedule = Schedule(schedule_date=case_date)
+        my_schedule.sio.sync_info_python_to_info(drop_ratio=0)
+        logger.info("sync info python to info is done")
+        return HttpResponse(json.dumps(result))
+    return HttpResponse("请求错误")
+
+
+def gantt_chart(request):
+    logger = Logger(__name__).get_logger()
+    if request.method == 'POST':
+        result = []
+        query_condition = json.loads(request.body)
+        case_date = query_condition['date']
+
+        logger.info("gantt chart and  case_date: " + case_date)
+
+        my_schedule = Schedule(schedule_date=case_date)
+        my_schedule.gantt_chart()
+        logger.info("gantt chart is done")
         return HttpResponse(json.dumps(result))
     return HttpResponse("请求错误")
